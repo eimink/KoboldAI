3e68cf338210f6f84afb664899ec8086c12d87a4
Teppo Kauppinen <teppo.kauppinen@traplightgames.com>
2015-08-29 15:51:09 +0300

    Custom raycasting result

---
+++ Assets/KoboldAI/Scripts/Util/RaycastResult.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace KoboldAI {
+	public class RaycastResult {
+
+		public bool doesHit;
+		public Vector2 position;
+		public Actor actor;
+
+	}
+}
